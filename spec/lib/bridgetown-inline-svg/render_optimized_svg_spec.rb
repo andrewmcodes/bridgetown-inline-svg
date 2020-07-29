7d8f400957e5be94a3bee8eb46a1a4432ce6c57d
Mike Rogers <me@mikerogers.io>
2020-07-29 21:34:26 +0100

    Adding specs to check the RenderOptimizedSvg is doing something useful

--- spec/lib/bridgetown-inline-svg/render_optimized_svg_spec.rb
+++ spec/lib/bridgetown-inline-svg/render_optimized_svg_spec.rb
@@ -6,4 +6,12 @@ describe BridgetownInlineSvg::RenderOptimizedSvg do
   let(:instance_class) { described_class.new(file_path, attributes) }
 
   it_behaves_like "RenderSvg"
+
+  describe "#call" do
+    subject { instance_class.call }
+
+    it "removes comment within SVG" do
+      expect(subject).to_not include('<!-- comment -->')
+    end
+  end
 end
--- spec/lib/bridgetown-inline-svg/render_svg_spec.rb
+++ spec/lib/bridgetown-inline-svg/render_svg_spec.rb
@@ -6,4 +6,12 @@ describe BridgetownInlineSvg::RenderSvg do
   let(:instance_class) { described_class.new(file_path, attributes) }
 
   it_behaves_like "RenderSvg"
+
+  describe "#call" do
+    subject { instance_class.call }
+
+    it "keeps comment within SVG" do
+      expect(subject).to include('<!-- comment -->')
+    end
+  end
 end
